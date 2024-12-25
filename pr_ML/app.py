fad34f5e3b7cf0fad212d273a8362f925029a2cc
Nguyễn Huy Hoàng <122038053+Hoo3g@users.noreply.github.com>
2024-12-25 23:26:50 +0700

    Update app

--- pr_ML/app.py
+++ pr_ML/app.py
@@ -77,5 +77,7 @@ def predict():
 
     return jsonify({"result": f"Ước tính mức độ béo phì: {result}"})
     
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
